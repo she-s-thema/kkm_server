481ba964db9f57cb2e975faf48281c4054a24da9
kimhyerin0909 <khyerin0909@gmail.com>
2022-08-01 23:19:02 +0900

    Feat - check if the id is a ggm user or not
    카카오 로그인 후 로그인된 사용자가 꼬꼬막 유저 DB에 있는지 검사한 후 있다면 유저 정보를 반환하고, 없다면 회원가입 시그널을 보냅니다.

--- src/main/java/com/example/kkm_back/Controller/OAuthController.java
+++ src/main/java/com/example/kkm_back/Controller/OAuthController.java
@@ -1,27 +1,37 @@
 package com.example.kkm_back.Controller;
+import com.example.kkm_back.Domain.User;
+import com.example.kkm_back.Repository.UserRepository;
 import com.example.kkm_back.Service.KakaoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
 public class OAuthController {
     @Autowired
     private KakaoService kakaoService;
+    @Autowired
+    private UserRepository userRepository;
+    @RequestMapping(value = "/user/kakaoLogin", method = RequestMethod.POST)
+    public Map<String, Object> KakaoLogin(@RequestParam(value="code", required = true) String code) throws Exception {
+        String access_Token = kakaoService.getAccessToken(code); // 액세스 토큰 얻기
+        Map<String, Object> userInfo = kakaoService.getUserInfo(access_Token); // 액세스 토큰으로 카카오 사용자 정보 얻기
+        Long k_id = Long.parseLong(String.valueOf(userInfo.get("id"))); // Object to long
+        Map<String, Object> exist = userRepository.isExist(k_id); // user table에 존재하는가?
 
-    @RequestMapping("/kakaoLogin")
-    public String KakaoLogin(@RequestParam(value="code", required = false) String code) throws Exception {
-        System.out.println("받은 인가코드 : "+ code);
-        String access_Token = kakaoService.getAccessToken(code);
-        System.out.println("액세스 토큰 : "+access_Token);
-
-        Map<String, Object> userInfo = kakaoService.getUserInfo(access_Token);
-
-        System.out.println(userInfo);
-
-        return "login success";
+        if(exist == null) { // 회원 가입이 안 되어 있다면
+            Map<String, Object> needMoreInfo = new HashMap<>();
+            needMoreInfo.put("id", 1);
+            needMoreInfo.put("message", "NEED MORE INFO");
+            return needMoreInfo;
+        } else {
+            return userInfo;
+        }
     }
 }
