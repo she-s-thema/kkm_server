d52ab385bef0d62ee05e4558a721fea708e0adb0
kimhyerin0909 <khyerin0909@gmail.com>
2022-08-03 22:59:15 +0900

    Feat - separate the api in two
    기존에 있던 로그인 api를 카카오의 유저 정보 발급 api와 카카오 아이디로 꼬꼬막 회원인지 아닌지 구별하는 api로 분리했습니다.

--- src/main/java/com/example/kkm_back/Controller/OAuthController.java
+++ src/main/java/com/example/kkm_back/Controller/OAuthController.java
@@ -5,8 +5,6 @@ import com.example.kkm_back.Service.KakaoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -15,20 +13,23 @@ public class OAuthController {
     private KakaoService kakaoService;
     @Autowired
     private UserRepository userRepository;
+
+    // kakao user info 받기
+    @RequestMapping(value = "/user/getKakaoUserInfo", method = RequestMethod.GET)
+    public Map<String, Object> GetKakaoUserInfo(@RequestParam(value = "code", required = true) String code) throws Exception {
+        Map<String, Object> userInfo = kakaoService.getUserInfo(kakaoService.getAccessToken(code)); // 액세스 토큰으로 카카오 사용자 정보 얻기
+        return userInfo;
+    }
+
+    // user가 회원가입 되어 있는지 체크 후 true라면 로그인 처리 + JWT token 발급 false라면 회원가입 절차
     @RequestMapping(value = "/user/kakaoLogin", method = RequestMethod.POST)
-    public Map<String, Object> KakaoLogin(@RequestParam(value="code", required = true) String code) throws Exception {
-        String access_Token = kakaoService.getAccessToken(code); // 액세스 토큰 얻기
-        Map<String, Object> userInfo = kakaoService.getUserInfo(access_Token); // 액세스 토큰으로 카카오 사용자 정보 얻기
-        Long k_id = Long.parseLong(String.valueOf(userInfo.get("id"))); // Object to long
-        Map<String, Object> exist = userRepository.isExist(k_id); // user table에 존재하는가?
+    public String  KakaoLogin(@RequestParam(value="k_id", required = true) long k_id) throws Exception {
+        Map<String, Object> exist = userRepository.isExist(k_id); // user table에 존재하면 존재하는 정보, 존재하지 않으면
 
         if(exist == null) { // 회원 가입이 안 되어 있다면
-            Map<String, Object> needMoreInfo = new HashMap<>();
-            needMoreInfo.put("id", 1);
-            needMoreInfo.put("message", "NEED MORE INFO");
-            return needMoreInfo;
-        } else { // 회원 가입이 되어있다면 유저 정보 보내기
-            return exist; // user_id, nickname, k_id, k_img_url, k_email
+            return "guest";
+        } else { // 회원 가입이 되어있다면 JWT token 보내줘야 함
+            return "user";
         }
     }
 
