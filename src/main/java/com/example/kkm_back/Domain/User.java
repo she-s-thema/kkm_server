f8b7c6da53b9a62d1829ab587c99090148515d8d
kimhyerin0909 <khyerin0909@gmail.com>
2022-08-18 14:18:11 +0900

    Fix - data type 변경

--- src/main/java/com/example/kkm_back/Domain/User.java
+++ src/main/java/com/example/kkm_back/Domain/User.java
@@ -6,13 +6,13 @@ import lombok.ToString;
 @Getter
 @ToString
 public class User {
-    private long user_id;
+    private String user_id;
     private String nickname;
-    private long k_id;
+    private String k_id;
     private String k_img_url;
     private String k_email;
 
-    public User(long user_id, String nickname, long k_id, String k_img_url, String k_email) {
+    public User(String user_id, String nickname, String k_id, String k_img_url, String k_email) {
         this.user_id = user_id;
         this.k_id = k_id;
         this.nickname = nickname;
