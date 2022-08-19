891012a9f98835251f3456d3b90d115e014fbbb2
kimhyerin0909 <khyerin0909@gmail.com>
2022-08-19 23:37:21 +0900

    Add - Security config

---
+++ src/main/java/com/example/kkm_back/Config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.example.kkm_back.Config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity // 기본적인 Web 보안 활성화
+public class SecurityConfig extends WebSecurityConfigurerAdapter { // 추가 설정
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http
+                .authorizeRequests() // HttpServletRequest를 사용하는 요청들에 대한 접근제한을 설정
+                .antMatchers("/user/**").permitAll(); // /api/hello에 대한 요청은 인증없이 접근을 허용
+//                .anyRequest().authenticated(); // 나머지 요청들은 모두 인증되어야 함
+    }
+}
