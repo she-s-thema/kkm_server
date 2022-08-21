cf2ecb17b1358a26aa3a5a995de1be1b84aa04b6
soohyuneeee <soohyuneeee@github.com>
2022-08-22 08:13:53 +0900

    post 추가

--- src/main/java/com/example/kkm_back/Controller/PostController.java
+++ src/main/java/com/example/kkm_back/Controller/PostController.java
@@ -1,2 +1,23 @@
-package com.example.kkm_back.Controller;public class postController {
+package com.example.kkm_back.Controller;
+
+
+import com.example.kkm_back.Domain.Post;
+import com.example.kkm_back.Repository.PostRepository;
+import com.sun.jdi.event.ExceptionEvent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PostController {
+    @Autowired
+    private PostRepository postRepository;
+
+    @RequestMapping(value = "/post")
+    public String Post(@ModelAttribute Post post) throws Exception{
+        postRepository.insertPost(post);
+        return "success";
+    }
 }
+
--- src/main/java/com/example/kkm_back/Repository/PostRepository.java
+++ src/main/java/com/example/kkm_back/Repository/PostRepository.java
@@ -1,2 +1,13 @@
-package com.example.kkm_back.Repository;public interface PostRepository {
+package com.example.kkm_back.Repository;
+
+import com.example.kkm_back.Domain.Post;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Mapper
+public interface PostRepository {
+    @Insert("insert into Post values (#{post_id}, #{post_owner_id}, #{image_1}, #{image_2}, #{image_3}, #{description}, #{write_time}, #{cost})")
+    void insertPost(Post post);
 }
