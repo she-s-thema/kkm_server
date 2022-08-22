7881ca953da92b46c0f835c628c4c510cb00170c
soohyuneeee <soohyuneeee@github.com>
2022-08-22 22:17:39 +0900

    get

--- src/main/java/com/example/kkm_back/Controller/PostController.java
+++ src/main/java/com/example/kkm_back/Controller/PostController.java
@@ -4,9 +4,11 @@ package com.example.kkm_back.Controller;
 import com.example.kkm_back.Domain.Post;
 import com.example.kkm_back.Repository.PostRepository;
 import com.sun.jdi.event.ExceptionEvent;
+import org.apache.logging.log4j.message.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,6 +20,12 @@ public class PostController {
     public String Post(@ModelAttribute Post post) throws Exception{
         postRepository.insertPost(post);
         return "success";
+    }
+
+    @RequestMapping(value = "/postlist", method = RequestMethod.GET)
+    public String Post(@ModelAttribute Post post) throws Exception{
+        postRepository.getAllMessages();
+        return
     }
 }
 
--- src/main/java/com/example/kkm_back/Repository/PostRepository.java
+++ src/main/java/com/example/kkm_back/Repository/PostRepository.java
@@ -3,11 +3,19 @@ package com.example.kkm_back.Repository;
 import com.example.kkm_back.Domain.Post;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.logging.log4j.message.Message;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 @Mapper
 public interface PostRepository {
     @Insert("insert into Post values (#{post_id}, #{post_owner_id}, #{image_1}, #{image_2}, #{image_3}, #{description}, #{write_time}, #{cost})")
     void insertPost(Post post);
+
+    @Select("SELECT * FROM Post JOIN users ON users.user_id = Post.post_owner_id WHERE post_id = #{post_id}")
+    List<Message> getAllMessages();
+
 }
