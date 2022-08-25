383fb53657f61ae37788db1436f01ec5b0efd160
soohyuneeee <soohyuneeee@github.com>
2022-08-25 22:13:25 +0900

    merge

--- src/main/java/com/example/kkm_back/Repository/PostRepository.java
+++ src/main/java/com/example/kkm_back/Repository/PostRepository.java
@@ -12,7 +12,7 @@ import java.util.List;
 public interface PostRepository {
     @Insert("insert into Post values (#{post_id}, #{post_owner_id}, #{image_1}, #{image_2}, #{image_3}, #{description}, #{write_time}, #{cost})")
     void insertPost(Post post);
-
+//hi
    // @Select("SELECT * FROM Post JOIN users ON users.user_id = Post.post_owner_id")
 
 
