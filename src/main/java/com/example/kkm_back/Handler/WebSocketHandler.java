6f28fadb6f2a008a068ed434f98059d14d9de676
kimhyerin0909 <khyerin0909@gmail.com>
2022-09-01 09:46:25 +0900

    Feat - make chat room and chatting

---
+++ src/main/java/com/example/kkm_back/Handler/WebSocketHandler.java
@@ -0,0 +1,30 @@
+package com.example.kkm_back.Handler;
+
+import com.example.kkm_back.Domain.ChatMessage;
+import com.example.kkm_back.Domain.ChatRoom;
+import com.example.kkm_back.Service.ChatService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class WebSocketHandler extends TextWebSocketHandler {
+    private final ObjectMapper objectMapper;
+    private final ChatService chatService;
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        String payload = message.getPayload();
+        log.info("{}", payload);
+        ChatMessage chatMessage = objectMapper.readValue(payload, ChatMessage.class);
+
+        ChatRoom chatRoom = chatService.findRoomById(chatMessage.getRoomId());
+        chatRoom.handlerActions(session, chatMessage, chatService);
+    }
+}
