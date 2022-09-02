8a31c4945d74bcb4f4b5929a0695ad3d6893fd5e
조수현 <soohyuneeee@josuhyeon-ui-MacBookPro.local>
2022-09-02 18:02:48 +0900

    Delete - Trade post_state

--- src/main/java/com/example/kkm_back/Domain/Trade.java
+++ src/main/java/com/example/kkm_back/Domain/Trade.java
@@ -18,9 +18,8 @@ public class Trade {
     private long trade_state;
     private java.sql.Timestamp start_time;
     private java.sql.Timestamp expired_time;
-    private long post_state;
 
-    public Trade(long trade_id, long post_id, long post_owner_id, long trader_id, long trade_type, long trade_cost, long trade_state,java.sql.Timestamp start_time, java.sql.Timestamp expired_time,long post_state){
+    public Trade(long trade_id, long post_id, long post_owner_id, long trader_id, long trade_type, long trade_cost, long trade_state,java.sql.Timestamp start_time, java.sql.Timestamp expired_time){
         this.trade_id=trade_id;
         this.post_id=post_id;
         this.post_owner_id=post_owner_id;
@@ -30,7 +29,6 @@ public class Trade {
         this.trade_state=trade_state;
         this.start_time=start_time;
         this.expired_time=expired_time;
-        this.post_state=post_state;
 
 
     }
