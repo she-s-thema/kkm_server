cb65b50789fe3b7aa7aa7a8f97a116157c55774a
kimhyerin0909 <khyerin0909@gmail.com>
2022-09-05 08:04:34 +0900

    Feat - chat server

--- src/main/java/com/example/kkm_back/Domain/ChatRoom.java
+++ src/main/java/com/example/kkm_back/Domain/ChatRoom.java
@@ -1,6 +1,7 @@
 package com.example.kkm_back.Domain;
 
 import com.example.kkm_back.Service.ChatService;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.web.socket.WebSocketSession;
@@ -9,21 +10,22 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Getter
+//@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 public class ChatRoom {
     private String roomId;
-    private String name;
+    private String userId;
     private Set<WebSocketSession> sessions = new HashSet<>();
 
     @Builder
-    public ChatRoom(String roomId, String name) {
+    public ChatRoom(String roomId, String userId) {
         this.roomId = roomId;
-        this.name = name;
+        this.userId = userId;
     }
 
     public void handlerActions(WebSocketSession session, ChatMessage chatMessage, ChatService chatService) {
         if (chatMessage.getType().equals(ChatMessage.MessageType.ENTER)) {
             sessions.add(session);
-            chatMessage.setMessage(chatMessage.getSender() + "님이 입장했습니다.");
+            chatMessage.setMessage(chatMessage.getSenderId() + "님이 입장했습니다.");
         }
         sendMessage(chatMessage, chatService);
 
