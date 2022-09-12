d26df7e8ca6be379351985d00881fcafdc17fc59
조수현 <soohyuneeee@josuhyeon-ui-MacBookPro.local>
2022-09-13 00:55:34 +0900

    Style - port:80

--- src/main/java/com/example/kkm_back/Config/FileConfig.java
+++ src/main/java/com/example/kkm_back/Config/FileConfig.java
@@ -1,16 +1,16 @@
-package com.example.kkm_back.Config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.multipart.commons.CommonsMultipartResolver;
-
-@Configuration
-public class FileConfig {
-    @Bean
-    public CommonsMultipartResolver multipartResolver() {
-        CommonsMultipartResolver mr = new CommonsMultipartResolver();
-        mr.setMaxInMemorySize(52428800); //50메가래
-        mr.setDefaultEncoding("utf-8");
-        return mr;
-    }
-}
+//package com.example.kkm_back.Config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+//
+//@Configuration
+//public class FileConfig {
+//    @Bean
+//    public CommonsMultipartResolver multipartResolver() {
+//        CommonsMultipartResolver mr = new CommonsMultipartResolver();
+//        mr.setMaxInMemorySize(52428800); //50메가래
+//        mr.setDefaultEncoding("utf-8");
+//        return mr;
+//    }
+//}
--- src/main/java/com/example/kkm_back/Controller/FileUploadController.java
+++ src/main/java/com/example/kkm_back/Controller/FileUploadController.java
@@ -1,24 +1,24 @@
-package com.example.kkm_back.Controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
-
-@Controller
-public class FileUploadController {
-    @Autowired
-    FileService fs;
-
-    @RequestMapping("form")
-    public String form() {
-        return "uploadForm";
-    }
-
-    @PostMapping("upload")
-    public String upload(MultipartHttpServletRequest mul) {
-        fs.fileProcess(mul);
-        return "redirect:form";
-    }
-}
+//package com.example.kkm_back.Controller;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.multipart.MultipartHttpServletRequest;
+//
+//@Controller
+//public class FileUploadController {
+//    @Autowired
+//    FileService fs;
+//
+//    @RequestMapping("form")
+//    public String form() {
+//        return "uploadForm";
+//    }
+//
+//    @PostMapping("upload")
+//    public String upload(MultipartHttpServletRequest mul) {
+//        fs.fileProcess(mul);
+//        return "redirect:form";
+//    }
+//}
