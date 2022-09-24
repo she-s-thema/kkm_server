61007d67b32719907d467bcc4a525172f157e542
조수현 <soohyuneeee@josuhyeon-ui-MacBookPro.local>
2022-09-24 22:51:20 +0900

    Feat - 약속 업데이트, 리뷰 업데이트

--- src/main/java/com/example/kkm_back/Controller/ReviewController.java
+++ src/main/java/com/example/kkm_back/Controller/ReviewController.java
@@ -3,6 +3,7 @@ package com.example.kkm_back.Controller;
 import com.example.kkm_back.DAO.ReviewDAO;
 import com.example.kkm_back.Domain.Review;
 import com.example.kkm_back.Repository.ReviewRepository;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,5 +25,11 @@ public class ReviewController {
     @RequestMapping(value = "/review/{review_id}", method = RequestMethod.GET, produces = "application/json; charset=utf-8")
     public Review getReview(@PathVariable("review_id")long review_id)throws Exception{
         return reviewRepository.getReview(review_id);
+    }
+    @ResponseBody
+    @RequestMapping(value = "/review/update", method = RequestMethod.PUT, produces = "application/json; charset=utf-8")
+    public String reviewUpdate(@Param("review_id")long review_id,@Param("comment")String comment,@Param("write_time")java.sql.Timestamp write_time)throws Exception{
+        reviewRepository.updateReview(review_id, comment, write_time);
+        return "success";
     }
 }
--- src/main/java/com/example/kkm_back/Repository/ReviewRepository.java
+++ src/main/java/com/example/kkm_back/Repository/ReviewRepository.java
@@ -4,6 +4,7 @@ import com.example.kkm_back.Domain.Review;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -11,4 +12,8 @@ import org.springframework.stereotype.Repository;
 public interface ReviewRepository {
     @Select("select * from Review where review_id=#{review_id}")
     Review getReview(@Param("review_id")long review_id);
-}
+    @Update("update Review set comment=#{comment},write_time=#{write_time} where review_id=#{review_id}")
+    void updateReview(@Param("review_id")long review_id,@Param("comment")String comment,@Param("write_time")java.sql.Timestamp write_time);
+    }
+
+
