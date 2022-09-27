bf7f1091ed387add9beefd085c0175bcb01d347b
조수현 <soohyuneeee@josuhyeon-ui-MacBookPro.local>
2022-09-27 21:35:55 +0900

    Fix - 리뷰 포스트 정보 보이게

--- src/main/java/com/example/kkm_back/Repository/ReviewRepository.java
+++ src/main/java/com/example/kkm_back/Repository/ReviewRepository.java
@@ -17,8 +17,9 @@ public interface ReviewRepository {
     Review getReview(@Param("review_id")long review_id);
     @Update("update Review set comment=#{comment},write_time=#{write_time} where review_id=#{review_id}")
     void updateReview(@Param("review_id")long review_id,@Param("comment")String comment,@Param("write_time")java.sql.Timestamp write_time);
-    @Select("select Review.comment,Review.reviewer_id,Review.write_time,U.nickname,U.k_img_url FROM Review\n" +
+    @Select("select Review.comment,Review.reviewer_id,Review.write_time,U.nickname,U.k_img_url,P.image_1,P.title,P.post_id FROM Review\n" +
             "inner join Users U on Review.reviewer_id = U.user_id\n" +
+            "inner join Post P on Review.post_id = P.post_id\n"+
             "where reviewed_id=#{reviewed_id}")
     List<Map<String,Object>> getReviewInfo(@Param("reviewed_id")long reviewed_id);
     }
